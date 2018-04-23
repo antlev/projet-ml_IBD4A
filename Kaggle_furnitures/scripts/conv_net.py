0701100efccc8a4f3d0dde113b6d44f7c88b2a34
antoine <antoinelevy@hotmail.com>
2018-04-23 22:07:01 +0200

    update conv_net

--- Kaggle_furnitures/scripts/conv_net.py
+++ Kaggle_furnitures/scripts/conv_net.py
@@ -1,3 +1,4 @@
+import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -13,13 +14,15 @@ import datetime
 train_data_dir = '../data/train'
 validation_data_dir = '../data/validation'
 log_dir = "/media/antoine/Linux-1/git/projet-ml_IBD4A/Kaggle_furnitures/log_furnitures/"
+model_dir = "/media/antoine/Linux-1/git/projet-ml_IBD4A/Kaggle_furnitures/models"
+
 batch_size = 1400
 
 # Experiment name
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-# KLD
-# experiment_name = "R128_P1_scc-mse_C2D_16(3-3)mp(2-2)_128sm_" + st
+
+# experiment_name = "R128_P1_cc-rm_C2D_16-32-64(3-3)mp(2-2)_128sm_" + st
 experiment_name = "test" + st
 print(experiment_name)
 
@@ -36,31 +39,26 @@ model = Sequential()
 model.add(Conv2D(16, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(32, (4, 4)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(64, (4, 4)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-# model.add(Dense(64))
-# model.add(Activation('relu'))
+# model.add(Dense(128))
+# model.add(Activation('tanh'))
 # model.add(Dropout(0.5))
 model.add(Dense(128, activation='softmax'))
-# model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer='sgd',
               metrics=['accuracy'])
-# model.compile(loss='sparse_categorical_crossentropy',
-#               optimizer='rmsprop',
-#               metrics=['accuracy'])
-
-# model.compile(loss=kullback_leibler_divergence,
-#               optimizer=rmsprop(),
-#               metrics=['accuracy'])
 
 tb_callback = TensorBoard(log_dir + experiment_name)
+model_checkpoint = ModelCheckpoint(model_dir + experiment_name)
+
+callback_list = [tb_callback, model_checkpoint]
 
 # Generator
 train_datagen = ImageDataGenerator(
@@ -83,18 +81,10 @@ validation_generator = validation_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-batch=next(train_generator)
-X=batch[0]
-Y=batch[1]
-print(X.shape)
-print(Y.shape)
-
 model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
-    callbacks=[tb_callback],
+    callbacks=callback_list,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
-
-model.save_weights('first_try.h5')
