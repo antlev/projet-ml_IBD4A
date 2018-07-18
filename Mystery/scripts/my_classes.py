c73d3e38a8393e670a84256d5471a95ad4a3d393
antoine <antoinelevy@hotmail.com>
2018-07-18 22:08:45 +0200

    add fit_output1_sequencer

--- Mystery/scripts/my_classes.py
+++ Mystery/scripts/my_classes.py
@@ -13,20 +13,6 @@ class MysterySequencer(keras.utils.Sequence):
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
         return batch_x, keras.utils.to_categorical(batch_y, self.nb_classes)
 
-def all_diff_element(array, is_input):
-    if array.size > 1000:
-        array_size=1000
-    else:
-        array_size=array.size
-    elements=[]
-    for i in range((array_size)):
-        if array[i] not in elements:
-            if(is_input):
-                elements.append(array[i])
-            else:
-                elements.append(array[i][0])
-    return elements
-
 class MysterySequencer(keras.utils.Sequence):
     def __init__(self, x_set, y_set, nb_classes, batch_size, is_categorical):
         self.x, self.y = x_set, y_set
@@ -42,19 +28,37 @@ class MysterySequencer(keras.utils.Sequence):
             batch_y = keras.utils.to_categorical(batch_y, self.nb_classes)
         return batch_x, batch_y
 
-def all_diff_element(array, is_input):
-    if array.size > 1000:
-        array_size=1000
+class MysterySequencerOutput1(keras.utils.Sequence):
+    def __init__(self, x_set, y_set, nb_classes, batch_size):
+        self.x, self.y = x_set, y_set
+        self.nb_classes = nb_classes
+        self.batch_size = batch_size
+    def __len__(self) -> int:
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+    def __getitem__(self, idx) -> tuple:
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+        outputs = np.copy(batch_y)
+        outputs[outputs < 0] = np.float32(0.0)
+        batch_y = keras.utils.to_categorical(batch_y, self.nb_classes)
+        return batch_x, batch_y
+
+def sample(array):
+    print("data size = " + str(array.size))
+    print("data shape = " + str(array[0].size))
+    if array.size > 10:
+        array_size=10
     else:
         array_size=array.size
-    elements=[]
+    if array[0].size > 30:
+        array_size2=30
+    else:
+        array_size2=array[0].size
     for i in range((array_size)):
-        if array[i] not in elements:
-            if(is_input):
-                elements.append(array[i])
-            else:
-                elements.append(array[i][0])
-    return elements
+        print("data nb"+str(i))
+        for j in range((array_size2)):
+            print(str(array[i][j]) + "|",end='')
+        print("")
 
 
 activations_shortcut = {'sigmoid' : 'Si',
@@ -87,3 +91,22 @@ optimizer_shortcut = {'sgd' : 'sgd',
                       'Adamax' : 'adax',#
                       'Nadam' : 'nada',#
                       'TFOptimizer' : 'tf' }#
+
+
+def check_input(input):
+    for item in input:
+        j=0;
+        while(1):
+            gotOne = False
+            for i in range(5):
+                if item[i+j] == 1:
+                    if(gotOne):
+                        print("there is two 1")
+                    else:
+                        gotOne=True
+            j=j+15
+            if(gotOne == False):
+                print("The is no one")
+            if(j+5 < item.size):
+                break
+    return 1
